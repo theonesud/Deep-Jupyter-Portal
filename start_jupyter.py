15d0d6ad6743173a75bb720fbe67f4173d668c5f
Sudhanshu Passi <sudhanshupassi@dijkstra.local>
2019-02-14 10:57:12 +0530

    added time to start jupyter script

--- start_jupyter.py
+++ start_jupyter.py
@@ -1,10 +1,7 @@
 from subprocess import Popen, PIPE, STDOUT
 import subprocess
 import random
-
-remote = '`portal info deep_gpu -f remote`'
-ssh_run = "ssh {} '{{}}'".format(remote)
-token = int(random.random() * 100000000)
+import time
 
 
 def run(cmd):
@@ -12,28 +9,34 @@ def run(cmd):
     return p.stdout.read()
 
 
-# copy service file
-print('>>>> Pushing service file')
-subprocess.call('scp ./jupyter.service {}:/home/ubuntu/'.format(remote), shell=True)
+if __name__ == '__main__':
+    start = time.time()
+    remote = '`portal info deep_gpu -f remote`'
+    ssh_run = "ssh {} '{{}}'".format(remote)
+    token = int(random.random() * 100000000)
 
-# setting jupyter auth token
-print('>>>> Set jupyter token')
-subprocess.call(ssh_run.format("echo 'export JUPYTER_TOKEN={}' >> ~/.bashrc".format(token)),
-                shell=True)
+    # copy service file
+    print('{} >>>> Pushing service file'.format(time.time() - start))
+    subprocess.call('scp ./jupyter.service {}:/home/ubuntu/'.format(remote), shell=True)
 
-# start the service
-print('>>>> Running jupyter notebook')
-cmd = 'sudo mv jupyter.service /etc/systemd/system/ &&\
- sudo systemctl daemon-reload &&\
- sudo systemctl enable jupyter.service &&\
- sudo systemctl start jupyter'
-subprocess.call(ssh_run.format(cmd), shell=True)
+    # setting jupyter auth token
+    print('{} >>>> Set jupyter token'.format(time.time() - start))
+    subprocess.call(ssh_run.format("echo 'export JUPYTER_TOKEN={}' >> ~/.bashrc".format(token)),
+                    shell=True)
 
-# fetch instance ip
-print('>>>> Fetching the ip')
-ip = run("portal info deep_gpu -f ip").strip()
+    # start the service
+    print('{} >>>> Running jupyter notebook'.format(time.time() - start))
+    cmd = 'sudo mv jupyter.service /etc/systemd/system/ &&\
+     sudo systemctl daemon-reload &&\
+     sudo systemctl enable jupyter.service &&\
+     sudo systemctl start jupyter'
+    subprocess.call(ssh_run.format(cmd), shell=True)
 
-# open in browser and run sync
-print('>>>> Opening in browser and starting sync')
-url = 'http://{}:8888/?token={}'.format(ip, token)
-subprocess.call("open {} && portal channel deep_gpu".format(url), shell=True)
+    # fetch instance ip
+    print('{} >>>> Fetching the ip'.format(time.time() - start))
+    ip = run("portal info deep_gpu -f ip").strip()
+
+    # open in browser and run sync
+    print('{} >>>> Opening in browser and starting sync'.format(time.time() - start))
+    url = 'http://{}:8888/?token={}'.format(ip, token)
+    subprocess.call("open {} && portal channel deep_gpu".format(url), shell=True)
