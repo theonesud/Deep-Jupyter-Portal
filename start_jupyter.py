7e793fc6c36f600ce3e1f7efc595b6e2d598f94d
Sudhanshu Passi <sudhanshupassi@dijkstra.local>
2019-02-14 15:39:41 +0530

    added config to jupyter for token

--- start_jupyter.py
+++ start_jupyter.py
@@ -1,43 +1,38 @@
 from subprocess import Popen, PIPE, STDOUT
-import subprocess
 import random
 import time
 
 
 def run(cmd):
     p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-    return p.stdout.read()
+    return p.stdout.read().strip()
 
 
 if __name__ == '__main__':
     start = time.time()
-    remote = '`portal info deep_gpu -f remote`'
-    ssh_run = "ssh {} '{{}}'".format(remote)
     token = int(random.random() * 100000000)
+    ip = run('portal info deep_gpu -f ip')
+    ssh_run = "ssh ubuntu@{} '{{}}'".format(ip)
 
-    # copy service file
-    print('{} >>>> Pushing service file'.format(time.time() - start))
-    subprocess.call('scp ./jupyter.service {}:/home/ubuntu/'.format(remote), shell=True)
+    print('>>> Uploading configs')
+    o = run('echo "c.NotebookApp.token = \'{}\'" >> ./jupyter_notebook_config.py &&'
+            'scp ./jupyter.service ubuntu@{}:~/ &&'
+            'scp ./jupyter_notebook_config.py ubuntu@{}:~/ &&'
+            'rm ./jupyter_notebook_config.py'.format(token, ip, ip))
+    print(o)
 
-    # setting jupyter auth token
-    print('{} >>>> Set jupyter token'.format(time.time() - start))
-    subprocess.call(ssh_run.format("echo \"c.NotebookApp.token = '{}'\" >> ~/.jupyter/"
-                                   "jupyter_notebook_config.py".format(token)),
-                    shell=True)
+    print('>>> Starting jupyter')
+    o = run(ssh_run.format('sudo rm -r /etc/systemd/system/jupyter.service && '
+                           'sudo mv ~/jupyter.service /etc/systemd/system/ && '
+                           'sudo systemctl daemon-reload && '
+                           'sudo systemctl enable jupyter.service && '
+                           'rm -f ~/.jupyter/jupyter_notebook_config.py && '
+                           'mv ~/jupyter_notebook_config.py ~/.jupyter/ && '
+                           'sudo systemctl restart jupyter'))
+    print(o)
+    time.sleep(2)
 
-    # start the service
-    print('{} >>>> Running jupyter notebook'.format(time.time() - start))
-    cmd = 'sudo mv jupyter.service /etc/systemd/system/ &&\
-     sudo systemctl daemon-reload &&\
-     sudo systemctl enable jupyter.service &&\
-     sudo systemctl start jupyter'
-    subprocess.call(ssh_run.format(cmd), shell=True)
-
-    # fetch instance ip
-    print('{} >>>> Fetching the ip'.format(time.time() - start))
-    ip = run("portal info deep_gpu -f ip").strip()
-
-    # open in browser and run sync
-    print('{} >>>> Opening in browser and starting sync'.format(time.time() - start))
-    url = 'http://{}:8888/?token={}'.format(ip, token)
-    subprocess.call("open {} && portal channel deep_gpu".format(url), shell=True)
+    print('>>> Opening browser and start sync')
+    o = run('open http://{}:8888/?token={} &&'
+            'portal channel deep_gpu'.format(ip, token))
+    print(o)
